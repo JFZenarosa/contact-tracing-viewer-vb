d5083a284c0b75eeb1ad8c9130b6e8cf6f9ba4dc
JFZenarosa <jaireh62@gmail.com>
2022-06-19 10:35:50 +0800

    Open File btn is now opening an OpenFileDialog

--- contact-tracing-viewer-vb/Form1.vb
+++ contact-tracing-viewer-vb/Form1.vb
@@ -1,3 +1,16 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Private Sub openBtn_Click(sender As Object, e As EventArgs) Handles openBtn.Click
+        Dim form As StreamReader
+        Dim ofd As New OpenFileDialog
+        ofd.ShowDialog()
+
+        If ofd.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim contactForm As String = ofd.FileName
+            Dim filetext As String = File.ReadAllLines(contactForm As String)
+
+
+        End If
+    End Sub
 End Class
